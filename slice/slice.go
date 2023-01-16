23b9b2aaf8f4f07300d6abf1624c2d937211ca47
fadilaahmads <fadilahmad59@gmail.com>
2023-01-16 23:08:51 +0700

    Add more veggies element, create slice from veggies

--- slice/slice.go
+++ slice/slice.go
@@ -9,9 +9,29 @@ func main() {
 	// yang memiliki alamat memori yang sama.
 
 	// Inisialisasi slice
-	var veggies = []string{"Zuccini", "Spinach", "Kale", "Carrot"}
+	var veggies = []string{
+		"Zuccini",
+		"Spinach",
+		"Kale",
+		"Carrot",
+		"Aubergine",
+		"Daikon",
+		"Birds Eye Chili",
+		"Bean Sprouts",
+	}
 	fmt.Println(veggies)
 	for _, vegetable := range veggies {
 		fmt.Println(vegetable)
 	}
+
+	// pick veggies on index 0 to 4
+	fmt.Println("veggies on index 0 to 4", veggies[0:6])
+	// slice from slice
+	sVeggies := veggies[3:6]
+	fmt.Println("Slice from slice veggies:", sVeggies)
+
+	// Panjang slice
+	sliceSize := len(veggies)
+	fmt.Println("Slice Size:", sliceSize)
+
 }
