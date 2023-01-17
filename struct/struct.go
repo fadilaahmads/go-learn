f265b8e5484b30bddb98bffda9aabaf88166d855
fadilaahmads <fadilahmad59@gmail.com>
2023-01-17 22:07:40 +0700

    Add new struct

--- struct/struct.go
+++ struct/struct.go
@@ -13,6 +13,9 @@ type Student struct {
 	Person
 	subject []string
 }
+type College struct {
+	student []Student
+}
 
 func main() {
 	// Struct initialization
@@ -29,7 +32,18 @@ func main() {
 		"Embeded System",
 	}
 	mahasiswa1.subject = append(mahasiswa1.subject, availableSubject...)
+	fmt.Println("Mahasiswa1:", mahasiswa1)
 
-	fmt.Println(mahasiswa1)
+	var mhs2 = Person{name: "Stevi", age: 20}
+	var mahasiswa2 = Student{id: 187388490, Person: mhs2, subject: availableSubject}
+	fmt.Println("Mahasiswa2:", mahasiswa2)
+
+	allStudents := []Student{mahasiswa1, mahasiswa2}
+	var collegeStudents = College{
+		student: allStudents,
+	}
+	for _, student := range collegeStudents.student {
+		fmt.Println(student)
+	}
 
 }
