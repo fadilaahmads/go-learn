eaf9a3777bbd5b05775e6a097eb10917ef867104
fadilaahmads <fadilahmad59@gmail.com>
2023-01-17 13:45:44 +0700

    Add map

---
+++ map/map.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Map adalah tipe data asosiatif yang ada di Go, berbentuk key-value pair.
+	// Untuk setiap data (atau value) yang disimpan, disiapkan juga key-nya.
+	// Key harus unik, karena digunakan sebagai penanda (atau identifier) untuk pengaksesan value yang bersangkutan.
+	// Map mirip array/slice tapi aksesnya bukan dari indeks, pakai key-value pair
+
+	// Inisialisasi map
+	// map[tipe key]tipe value
+	var foods map[string]int{}
+	// insialisasi nilai dalam map
+	foods = map[string]int{} // biar gak nil
+
+	foods["Apple Pie"] = 20
+	foods["Burgir"] = 14
+	foods["Cake"] = 19
+	foods["Dimsum"] = 15
+	foods["Edamame"] = 5
+	foods["Falafel"] = 8
+	foods["Grape Juice"] = 3
+
+	fmt.Println(foods["Burgir"])
+
+}
