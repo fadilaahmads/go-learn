d7171af55de625ba0317f9bbfdacb62f9e3f073a
fadilaahmads <fadilahmad59@gmail.com>
2023-01-17 17:49:21 +0700

    Add pointer

---
+++ pointer/pointer.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Pointer adalah reference atau alamat memori.
+	// Variabel pointer berarti variabel yang berisi alamat memori suatu nilai.
+	// Sebagai contoh sebuah variabel bertipe integer memiliki nilai 4,
+	// maka yang dimaksud pointer adalah alamat memori di mana nilai 4 disimpan, bukan nilai 4 itu sendiri.
+
+	// Inisialisasi pointer
+	var varBiasa int = 1980
+	var varPointer *int = &varBiasa
+
+	fmt.Println("Nilai varBiasa:", varBiasa)
+	fmt.Println("Alamat memori varBiasa:", varPointer)
+
+}
