708c98b38d1ef2e9a2f05255612020eb1fdb7dd1
fadilaahmads <fadilahmad59@gmail.com>
2023-01-19 17:41:59 +0700

    Add goroutine desc

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -12,6 +12,12 @@ func Loop1(num int, message string) {
 }
 
 func main() {
+	// Goroutine mirip dengan thread, tapi sebenarnya bukan.
+	// Sebuah native thread bisa berisikan sangat banyak goroutine.
+	// Mungkin lebih pas kalau goroutine disebut sebagai mini thread.
+	// Goroutine sangat ringan, hanya dibutuhkan sekitar 2kB memori saja untuk satu buah goroutine.
+	// Eksekusi goroutine bersifat asynchronous, menjadikannya tidak saling tunggu dengan goroutine lain.
+
 	// Specify the number of logical processor cores
 	runtime.GOMAXPROCS(2)
 	go Loop1(10, "With goroutine")
