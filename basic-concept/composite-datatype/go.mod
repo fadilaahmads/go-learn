243d44adfff66faefb06176e7e80efd4bf51f640
fadilaahmads <fadilahmad59@gmail.com>
2024-02-11 14:28:56 +0700

    add composite-datatype

---
+++ basic-concept/composite-datatype/go.mod
@@ -0,0 +1,3 @@
+module composite-type
+
+go 1.21.6
---
+++ basic-concept/composite-datatype/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main(){
+  // Declaring an array
+  var odds [5]int
+  odds[0] = 1
+  odds[1] = 3
+  odds[2] = 5
+  odds[3] = 7
+  odds[4] = 9
+  even := [5]int{2,4,6,8,10}
+  
+  for i:= 0 ; i < len(odds); i++ {
+    fmt.Println("odds array: ", odds[i])
+  }
+
+  for _, j := range even{
+    fmt.Println("even array: ", j)
+  }
+
+  // Declaring slice
+  var odds_slice []int = odds[1:3]
+  for _, odds_slice_value := range odds_slice{
+    fmt.Println("odds slice: ", odds_slice_value)
+  }
+  even_slice := even[1:3]
+  for i:=0; i < len(even_slice); i++{
+    fmt.Println("even slice: ", even_slice[i])
+  }
+}
