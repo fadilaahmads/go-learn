2ffb3add297aad9cf9148569b748fa85c956fe88
fadilaahmads <fadilahmad59@gmail.com>
2024-06-02 23:04:48 +0700

    add database getter

--- learning-projects/todolist/databases/database.go
+++ learning-projects/todolist/databases/database.go
@@ -7,12 +7,12 @@ import (
 
   "github.com/jinzhu/gorm"
   _ "github.com/jinzhu/gorm/dialects/mysql"
-	"github.com/yourusername/todo-app/models"
+/* 	"gotodolist/models" */
 )
 
-var DB *gorm.db 
+var DB *gorm.DB 
 
-func DBInit()  {
+func Init() *gorm.DB {
   dbUser := os.Getenv("DB_USER")
   dbPassword := os.Getenv("DB_PASS")
   dbHost := os.Getenv("DB_HOST")
@@ -30,6 +30,14 @@ func DBInit()  {
   }
 
   // Automatically migrate the schema, to keep your schema updated.
-	// DB.AutoMigrate(&models.Todo{})
-
+  //DB.AutoMigrate(&models.Todo{})
+  return DB
 }
+
+func GetDB() *gorm.DB{
+  db := Init()
+  if db == nil {
+    log.Panicln("Failed to initialize database connection")
+  }
+  return db
+ }
