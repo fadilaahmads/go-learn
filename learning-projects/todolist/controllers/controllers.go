8fdd314323ce91689e15c90e58add5cef0c6a901
fadilaahmads <fadilahmad59@gmail.com>
2024-06-05 00:09:17 +0700

    create controller for get todo by id

--- learning-projects/todolist/controllers/controllers.go
+++ learning-projects/todolist/controllers/controllers.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"gotodolist/services"
 	"gotodolist/utils"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,4 +22,16 @@ func GetTodos(c *gin.Context){
   utils.ResponseWithJSON(c, http.StatusOK, todos)
 }
 
-
+func GetTodoById(c *gin.Context){
+  todoId := c.Param("id")
+  intTodoId, err := strconv.Atoi(todoId)
+  if err != nil{
+    log.Panicln("Failed to convert ID")
+    utils.ResponseWithError(c, http.StatusInternalServerError, err.Error())
+  }
+  todo, err := services.GetTodosById(intTodoId)
+  if err != nil {
+    utils.ResponseWithError(c, http.StatusInternalServerError, err.Error())
+  }
+  utils.ResponseWithJSON(c, http.StatusOK, todo)
+}
