4893d942225341e6c6409de449245257d2dd55ec
fadilaahmads <fadilahmad59@gmail.com>
2024-06-04 21:59:15 +0700

    move database getter to outside of the functions

--- learning-projects/todolist/services/services.go
+++ learning-projects/todolist/services/services.go
@@ -5,11 +5,15 @@ import (
 	"gotodolist/databases"
 	"gotodolist/models"
 	"log"
+  "github.com/jinzhu/gorm"
+  _ "github.com/jinzhu/gorm/dialects/mysql"
+
 )
+
+var db *gorm.DB = databases.GetDB()  
 
 func GetTodos()([]models.Todo, error){
   var todos []models.Todo
-  db := databases.GetDB()
   if db == nil {
     log.Println("Database connection is not initialize")
     return nil, fmt.Errorf("Database is not initialize")
