88df819de178af1d5ec556bc96bd0de8c844161b
fadilaahmads <fadilahmad59@gmail.com>
2024-06-09 20:20:43 +0700

    add validation & sanitization functions

---
+++ learning-projects/todolist/utils/security.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"errors"
+	"regexp"
+)
+
+func ValidateSanitizedTodo(title string) (string, error){ 
+  if len(title) == 0 || len(title) <255 {
+    return "", errors.New("title must be between 1 and 255 characters")
+  }
+  sanitizedTitle := regexp.MustCompile(`[^\w\s]`).ReplaceAllString(title, "")
+
+  return sanitizedTitle, nil
+}
+
+func ValidateID(id int) error{
+  if id < 0 || id > 1000 {
+    return errors.New("Invalid ID: must between 1 and 1000")
+  }
+  return nil
+}
