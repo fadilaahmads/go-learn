ce0c1382f3f675e6274cfd3f3941de105d9187f8
fadilaahmads <fadilahmad59@gmail.com>
2024-06-10 22:34:28 +0700

    remove ValidateCompletion function, splitting condition checking ValidateSanitizedTodo function

--- learning-projects/todolist/utils/security.go
+++ learning-projects/todolist/utils/security.go
@@ -6,24 +6,20 @@ import (
 )
 
 func ValidateSanitizedTodo(title string) (string, error){ 
-  if len(title) == 0 || len(title) <255 {
-    return "", errors.New("title must be between 1 and 255 characters")
+  if len(title) == 0 || len(title) > 255 {
+    return "", errors.New("title must be more than 0 character")
   }
-  sanitizedTitle := regexp.MustCompile(`[^\w\s]`).ReplaceAllString(title, "")
-
+  if len(title) > 255 {
+    return "", errors.New("title must be less than 255 characters")
+  }
+  sanitizedTitle := regexp.MustCompile(`[^\w\s\-\'.,]`).ReplaceAllString(title, "")
+  
   return sanitizedTitle, nil
 }
 
 func ValidateID(id int) error{
   if id < 0 || id > 1000 {
     return errors.New("Invalid ID: must between 1 and 1000")
-  }
-  return nil
-}
-
-func ValidateCompletion(status int) error{
-  if status != 0 || status != 1 {
-    return errors.New("Completion must either 1 or 0")
   }
   return nil
 }
